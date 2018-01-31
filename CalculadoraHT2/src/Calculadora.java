bd9ae0c7f30d59470fc511465a026ec5e0d9a16f
DESKTOP-9JNVIM8\Antonio <reytono@gmail.com>
2018-01-30 20:11:40 -0600

    prueba1

---
+++ CalculadoraHT2/src/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Calculadora implements CalculadoraI {
+    public String calcular(String esp){
+        return"";
+    }
+}
